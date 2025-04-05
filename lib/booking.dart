bb3e9291e73db04982ed1945c7623a6878e86339
taksh <taksh5834@gmail.com>
2025-04-05 11:53:33 +0530

    Share Done

--- lib/booking.dart
+++ lib/booking.dart
@@ -1,27 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:get/get.dart';
-import 'package:hall_2/controller/clear_data.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:share_plus/share_plus.dart';
 import 'dart:math';
-import 'package:firebase_core/firebase_core.dart';
 
-import 'controller/booking.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseFirestore.instance.settings = const Settings(
-    persistenceEnabled: true,
-  );
-  runApp(const MyApp());
-}
-
-final _Formkey = GlobalKey<FormState>();
-
-final BookingController bookingController = Get.put(BookingController());
-final ClearData clearData = Get.put(ClearData());
+final _formKey = GlobalKey<FormState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -48,6 +32,9 @@ class _BookingScreenState extends State<BookingScreen> {
   DateTime? _startDate;
   DateTime? _endDate;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _contactController = TextEditingController();
 
@@ -57,7 +44,6 @@ class _BookingScreenState extends State<BookingScreen> {
             .collection('bookings')
             .where('startDate', isLessThan: end.toIso8601String())
             .get();
-
     for (var doc in bookings.docs) {
       DateTime bookedStart = DateTime.parse(doc['startDate']);
       DateTime bookedEnd = DateTime.parse(doc['endDate']);
@@ -78,7 +64,7 @@ class _BookingScreenState extends State<BookingScreen> {
     ).join();
   }
 
-  double _calculateRent(DateTime startDate, DateTime endDate) {
+  double _calculateRent(DateTime startDate, DateTime endDate, bool isFullDay) {
     int daysBetween = startDate.difference(DateTime.now()).inDays;
     int totalDays = endDate.difference(startDate).inDays + 1;
 
@@ -115,7 +101,11 @@ class _BookingScreenState extends State<BookingScreen> {
       return;
     }
 
-    double finalRent = _calculateRent(_startDate!, _endDate!);
+    bool isFullDay =
+        _startDate!.isAtSameMomentAs(_endDate!)
+            ? await _showHalfFullDayDialog()
+            : true;
+    double finalRent = _calculateRent(_startDate!, _endDate!, isFullDay);
     String bookingCode = _generateRandomCode();
 
     try {
@@ -126,11 +116,25 @@ class _BookingScreenState extends State<BookingScreen> {
         'name': _nameController.text,
         'contact': _contactController.text,
         'finalRent': finalRent,
+        'isFullDay': isFullDay,
       });
+
+      String bookingDetails =
+          "📅 Hall Booking Confirmation\n"
+          "👤 Name: ${_nameController.text}\n"
+          "📞 Contact: ${_contactController.text}\n"
+          "🗓️ Start Date: ${DateFormat('dd/MM/yyyy').format(_startDate!)}\n"
+          "🗓️ End Date: ${DateFormat('dd/MM/yyyy').format(_endDate!)}\n"
+          "🔖 Booking Code: $bookingCode\n"
+          "💰 Total Rent: ₹$finalRent\n"
+          "📌 Booking Type: ${isFullDay ? 'Full Day' : 'Half Day'}\n"
+          "✅ Your hall booking is confirmed!";
 
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Booking confirmed ✅ Total Rent: ₹$finalRent')),
       );
+
+      Share.share(bookingDetails);
     } catch (e) {
       ScaffoldMessenger.of(
         context,
@@ -138,176 +142,197 @@ class _BookingScreenState extends State<BookingScreen> {
     }
   }
 
+  Future<bool> _showHalfFullDayDialog() async {
+    return await showDialog<bool>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Choose Booking Type'),
+              content: const Text(
+                'Do you want to book for a full day or half day?',
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, false),
+                  child: const Text('Half Day'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context, true),
+                  child: const Text('Full Day'),
+                ),
+              ],
+            );
+          },
+        ) ??
+        true; // Default to full day if no selection is made
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(title: const Text('Hall Booking')),
-      body: Column(
-        children: [
-          Obx(() {
-            return TableCalendar(
-              firstDay: DateTime.utc(2020, 1, 1),
-              lastDay: DateTime.utc(2101, 12, 31),
-              focusedDay: bookingController.focusedDay.value,
-              calendarFormat: bookingController.calendarFormat.value,
-              selectedDayPredicate:
-                  (day) => isSameDay(bookingController.selectedDay.value, day),
-
-              onDaySelected: (selectedDay, focusedDay) {
-                bookingController.selectDate(selectedDay);
-                bookingController.focusedDay.value = focusedDay;
-              },
-
-              onFormatChanged: (format) {
-                bookingController.changeFormat(format);
-              },
-
-              onPageChanged: (focusedDay) {
-                bookingController.focusedDay.value = focusedDay;
-              },
-
-              rangeStartDay: bookingController.startDate.value,
-              rangeEndDay: bookingController.endDate.value,
-
-              calendarStyle: CalendarStyle(
-                rangeHighlightColor: Colors.blueAccent.withOpacity(0.5),
-                selectedDecoration: BoxDecoration(
-                  color: Colors.blue,
-                  shape: BoxShape.circle,
-                ),
-                todayDecoration: BoxDecoration(
-                  color: Colors.orange,
-                  shape: BoxShape.circle,
+      appBar: AppBar(
+        title: Text(
+          'Hall Booking',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        elevation: 4,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TableCalendar(
+                  firstDay: DateTime.utc(2020, 1, 1),
+                  lastDay: DateTime.utc(2101, 12, 31),
+                  focusedDay: _focusedDay,
+                  calendarFormat: _calendarFormat,
+                  selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                  onDaySelected: (selectedDay, focusedDay) {
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      if (_startDate == null ||
+                          (_startDate != null && _endDate != null)) {
+                        _startDate = selectedDay;
+                        _endDate = null;
+                      } else {
+                        _endDate =
+                            selectedDay.isAfter(_startDate!)
+                                ? selectedDay
+                                : _startDate;
+                      }
+                    });
+                  },
+                  rangeStartDay: _startDate,
+                  rangeEndDay: _endDate,
+                  calendarStyle: CalendarStyle(
+                    rangeHighlightColor: Colors.blueAccent.withOpacity(0.5),
+                    selectedDecoration: BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                    ),
+                    todayDecoration: BoxDecoration(
+                      color: Colors.orange,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
                 ),
               ),
-            );
-          }),
-
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _Formkey,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "Selected Dates",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.blueAccent.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(10),
+              ),
               child: Column(
                 children: [
-                  Row(
-                    children: [
-                      Text(
-                        bookingController.startDate.value != null
-                            ? "Start Date: ${DateFormat('dd/MM/yyyy').format(bookingController.startDate.value!)}"
-                            : "Start Date: Not Selected",
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ],
+                  Text(
+                    _startDate != null
+                        ? "Start Date: ${DateFormat('dd/MM/yyyy').format(_startDate!)}"
+                        : "Start Date: Not Selected",
+                    style: const TextStyle(fontSize: 16, color: Colors.blue),
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        bookingController.startDate.value != null
-                            ? "End Date: ${DateFormat('dd/MM/yyyy').format(bookingController.endDate.value!)}"
-                            : "Start Date: Not Selected",
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ],
+                  Text(
+                    _endDate != null
+                        ? "End Date: ${DateFormat('dd/MM/yyyy').format(_endDate!)}"
+                        : "End Date: Not Selected",
+                    style: const TextStyle(fontSize: 16, color: Colors.blue),
                   ),
-
-                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
                   TextFormField(
                     controller: _nameController,
+                    keyboardType: TextInputType.name,
                     decoration: InputDecoration(
                       labelText: 'Full Name',
-                      labelStyle: TextStyle(color: Colors.blue, fontSize: 16),
                       prefixIcon: Icon(Icons.person, color: Colors.blue),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.blue, width: 2),
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 12,
-                        horizontal: 16,
                       ),
                     ),
-                    validator: (value) {
-                      return (value == null || value.isEmpty)
-                          ? "Enter Name"
-                          : null;
-                    },
+                    validator:
+                        (value) =>
+                            value == null || value.isEmpty
+                                ? "Enter Name"
+                                : null,
                   ),
                   const SizedBox(height: 10),
                   TextFormField(
+                    maxLength: 10,
                     controller: _contactController,
                     keyboardType: TextInputType.phone,
                     decoration: InputDecoration(
                       labelText: 'Contact No.',
-                      labelStyle: TextStyle(color: Colors.blue, fontSize: 16),
                       prefixIcon: Icon(Icons.phone, color: Colors.blue),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
                       ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.blue, width: 2),
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 14,
-                        horizontal: 16,
-                      ),
                     ),
-                    validator: (value) {
-                      return (value == null || value.isEmpty)
-                          ? "Enter Contact No."
-                          : null;
-                    },
+                    validator:
+                        (value) =>
+                            value == null || value.isEmpty
+                                ? "Enter Contact No."
+                                : null,
                   ),
-
                   const SizedBox(height: 20),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
+                  Visibility(
+                    visible: _startDate == _endDate,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _showHalfFullDayDialog();
+                      },
+                      child: Text("Choose Day Type"),
                     ),
-                    onPressed: () {
-                      _bookHall();
-                      _Formkey.currentState!.validate();
-                      clearData.clearInputs();
-                    },
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const Icon(
-                          Icons.event_available,
-                          color: Colors.white,
-                        ), // Icon
-                        const SizedBox(width: 8), // Spacing
-                        const Text(
-                          'Book Hall',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _bookHall,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        padding: EdgeInsets.symmetric(vertical: 14),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                      ],
+                      ),
+                      child: Text(
+                        'Book Hall',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,17 +1,14 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'booking.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hall Booking',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: BookingScreen(),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
+  runApp(const MyApp());
 }
